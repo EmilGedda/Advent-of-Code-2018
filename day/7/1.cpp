3ea07aae5c2c864d9e16bbe08029f7d8028a856d
Emil Gedda <emil.gedda@emilgedda.se>
2018-12-23 13:47:05 +0100

    day 7 part one

---
+++ day/7/1.cpp
@@ -0,0 +1,41 @@
+#include <array>
+#include <unordered_map>
+#include <set>
+#include <string>
+#include <vector>
+#include <cassert>
+#include <iostream>
+
+auto solve() {
+
+  std::unordered_map<char, std::set<char>> dependents;
+  std::unordered_map<char, std::set<char>> dependencies;
+  std::set<char> roots;
+
+  for(auto c = 'A'; c <= 'Z'; c++)
+    roots.insert(c);
+
+  char a, b;
+  auto format = "Step %c must be finished before step %c can begin.\n";
+
+  while(std::scanf(format, &a, &b) == 2) {
+    dependents[a].insert(b);
+    dependencies[b].insert(a);
+    roots.erase(b);
+  }
+
+  // Kahn's algorithm - topological sorting
+  std::string plan_order;
+  while(!roots.empty()) {
+    auto step = roots.extract(roots.begin()).value();
+    plan_order += step;
+    for(auto m: dependents[step]) {
+      dependencies[m].erase(step);
+      if(dependencies[m].empty())
+        roots.insert(m);
+    }
+  }
+
+  std::cout << plan_order << '\n';
+  return 0;
+}
