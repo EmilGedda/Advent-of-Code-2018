0ed5db33c0fa27d677a4f82c88225d3672df27a4
Emil Gedda <emil.gedda@emilgedda.se>
2018-12-26 13:29:10 +0100

    minor cleanup

--- day/14/1.cpp
+++ day/14/1.cpp
@@ -4,12 +4,12 @@
 constexpr auto n = 793061;
 
 auto solve() {
-  std::vector<int> recipes = {3, 7};
-  int elf_a = 0, elf_b = 1;
+  std::vector recipes = {3, 7};
+  auto elf_a = 0, elf_b = 1;
   while(recipes.size() < n + 10) {
     auto sum = recipes[elf_a] + recipes[elf_b];
     if(sum >= 10)
-      recipes.push_back((sum / 10) % 10);
+      recipes.push_back(1);
     recipes.push_back(sum % 10);
     elf_a = (elf_a + recipes[elf_a] + 1) % recipes.size();
     elf_b = (elf_b + recipes[elf_b] + 1) % recipes.size();
--- day/14/2.cpp
+++ day/14/2.cpp
@@ -6,14 +6,14 @@
 constexpr std::array ending = { 7, 9, 3, 0, 6, 1 };
 
 auto solve() {
-  std::vector<int> recipes = {3, 7};
+  std::vector recipes = {3, 7};
   auto elf_a = 0, elf_b = 1;
 
   while(!std::includes(recipes.end() - ending.size() - 1, recipes.end()
                      , ending.begin(), ending.end())) {
     auto sum = recipes[elf_a] + recipes[elf_b];
     if(sum >= 10)
-      recipes.push_back((sum / 10) % 10);
+      recipes.push_back(1);
     recipes.push_back(sum % 10);
     elf_a = (elf_a + recipes[elf_a] + 1) % recipes.size();
     elf_b = (elf_b + recipes[elf_b] + 1) % recipes.size();
