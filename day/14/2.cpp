08229fb94c4a460346714520b9edcedc377fbc54
Emil Gedda <egedda@kth.se>
2019-01-27 13:05:02 +0100

    day 14 clean up

--- day/14/2.cpp
+++ day/14/2.cpp
@@ -1,7 +1,6 @@
 #include <vector>
 #include <array>
 #include <algorithm>
-#include <iostream>
 
 constexpr std::array ending = { 7, 9, 3, 0, 6, 1 };
 
@@ -9,8 +8,7 @@ auto solve() {
   std::vector recipes = {3, 7};
   auto elf_a = 0, elf_b = 1;
 
-  while(!std::includes(recipes.end() - ending.size() - 1, recipes.end()
-                     , ending.begin(), ending.end())) {
+  while(!std::includes(recipes.end() - ending.size() - 1, recipes.end(), ending.begin(), ending.end())) {
     auto sum = recipes[elf_a] + recipes[elf_b];
     if(sum >= 10)
       recipes.push_back(1);
@@ -19,7 +17,5 @@ auto solve() {
     elf_b = (elf_b + recipes[elf_b] + 1) % recipes.size();
   }
 
-  std::cout << recipes.size() - ending.size() - 1 << '\n';
-
-  return 0;
+  return recipes.size() - ending.size() - 1;
 }
